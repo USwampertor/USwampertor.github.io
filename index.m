0caeed1cc428f8a1415bd48da5d9310cadad78ed
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2019-11-27 17:59:05 -0600

    *Adding template

--- index.m
+++
@@ -1,1 +0,0 @@
-<script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#46b798', 'T6T5OKMS');kofiwidget2.draw();</script> 
